d71f13d5ec701c89762f640ce6254ea3c297fd79
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2022-12-19 10:28:29 +0700

    Create card_win.dart

---
+++ hunt_the_mafia/lib/views/widgets/card_win.dart
@@ -0,0 +1,15 @@
+part of 'widgets.dart';
+
+class WinCard extends StatefulWidget {
+  const WinCard({Key? key}) : super(key: key);
+
+  @override
+  State<WinCard> createState() => _WinCardState();
+}
+
+class _WinCardState extends State<WinCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
