174b8291478d2632206db678c4716279d62c5de5
Bryan <71095749+bryanless@users.noreply.github.com>
2022-12-19 13:45:57 +0700

    Remove unused imports

--- hunt_the_mafia/lib/views/pages/pages.dart
+++ hunt_the_mafia/lib/views/pages/pages.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hunt_the_mafia/theme/space.dart';
 import 'package:hunt_the_mafia/views/widgets/widgets.dart';
 import 'package:lottie/lottie.dart';
-import 'package:hunt_the_mafia/theme/shape.dart';
-
-import '../../theme/space.dart';
 
 part "game.dart";
 part 'page_game_room.dart';
