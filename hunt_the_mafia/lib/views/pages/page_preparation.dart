c3a97df3a04d94ae3b34c67e0cb365c341444e23
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2022-12-30 09:39:28 +0700

    add counter

--- hunt_the_mafia/lib/views/pages/page_preparation.dart
+++ hunt_the_mafia/lib/views/pages/page_preparation.dart
@@ -12,6 +12,66 @@ class PreparationPage extends StatefulWidget {
 class _PreparationPageState extends State<PreparationPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    int _count = 0;
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.all(Space.large),
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Column(
+            children: [
+              SizedSpacer.vertical(space: Space.medium),
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      'Room Code',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                    SizedSpacer.vertical(space: Space.medium),
+                    Text(
+                      'N1310',
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ],
+                ),
+              ),
+              SizedSpacer.vertical(space: Space.medium),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _count++;
+                    });
+                  },
+                  backgroundColor: Colors.white,
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.black,
+                  ),
+                ),
+                Text('$_count', style: const TextStyle(fontSize: 60.0)),
+                FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _count--;
+                    });
+                  },
+                  backgroundColor: Colors.white,
+                  child: const Icon(Icons.remove,
+                      color: Colors.black),
+                ),
+              ],
+            ),
+          ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
