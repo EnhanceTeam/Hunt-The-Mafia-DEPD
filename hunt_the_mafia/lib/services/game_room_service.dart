7ca4f1505193cdd31167c56f63be71eeb852f04d
Bryan <71095749+bryanless@users.noreply.github.com>
2022-12-31 23:57:36 +0700

    Implement game room page

--- hunt_the_mafia/lib/services/game_room_service.dart
+++ hunt_the_mafia/lib/services/game_room_service.dart
@@ -1,6 +1,41 @@
 part of 'services.dart';
 
-class GameRoomServie {
+class GameRoomService {
+  static Future<bool> isPlayerHost(String roomId, String nickname) async {
+    CollectionReference roomsCollectionReference =
+        FirebaseFirestore.instance.collection("rooms");
+    DocumentReference roomDocRef = roomsCollectionReference.doc(roomId);
+    DocumentSnapshot roomDocSnapshot = await roomDocRef.get();
+
+    String playerNicknamesField = "playerNicknames";
+
+    if (roomDocSnapshot.exists) {
+      Map<String, dynamic> data =
+          roomDocSnapshot.data() as Map<String, dynamic>;
+      List<dynamic> playerNicknames = data[playerNicknamesField] ?? [];
+
+      if (!playerNicknames.contains(nickname)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  static Future<bool> removeRoom(String roomId) async {
+    CollectionReference roomsCollectionReference =
+        FirebaseFirestore.instance.collection("rooms");
+    DocumentReference roomDocRef = roomsCollectionReference.doc(roomId);
+
+    roomDocRef.delete().then((value) {
+      return true;
+    }).catchError((e) {
+      debugPrint(e);
+    });
+
+    return false;
+  }
+
   static Future<bool> removePlayer(String roomId, String nickname) async {
     CollectionReference roomsCollectionReference =
         FirebaseFirestore.instance.collection("rooms");
--- hunt_the_mafia/lib/views/pages/page_game_room.dart
+++ hunt_the_mafia/lib/views/pages/page_game_room.dart
@@ -10,15 +10,31 @@ class GameRoomPage extends StatefulWidget {
 }
 
 class _GameRoomPageState extends State<GameRoomPage> {
+  bool isPlayerEnough = false;
+
+  void checkPlayerAmount(int playerAmount) {
+    if (playerAmount > 1) {
+      isPlayerEnough = true;
+    }
+
+    isPlayerEnough = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as GameRoomPageArguments;
 
     return WillPopScope(
-      onWillPop: () {
-        GameRoomServie.removePlayer(args.roomId, args.nickname);
+      onWillPop: () async {
+        bool isPlayerHost =
+            await GameRoomService.isPlayerHost(args.roomId, args.nickname);
 
+        if (!isPlayerHost) {
+          GameRoomService.removePlayer(args.roomId, args.nickname);
+        }
+
+        if (!mounted) return Future.value(false);
         Navigator.pop(context, false);
 
         return Future.value(false);
@@ -45,7 +61,7 @@ class _GameRoomPageState extends State<GameRoomPage> {
               SizedSpacer.vertical(space: Space.medium),
               Expanded(
                 child: Container(
-                  padding: const EdgeInsets.all(Space.small),
+                  padding: const EdgeInsets.symmetric(horizontal: Space.small),
                   decoration: BoxDecoration(
                       color: const Color(0xFF311A46),
                       borderRadius:
@@ -63,10 +79,11 @@ class _GameRoomPageState extends State<GameRoomPage> {
                         } else if (snapshot.hasData) {
                           var data = snapshot.data!.data();
                           var hostname = data!['hostname'];
-                          var playerNicknames = data['playerNicknames'];
+                          var playerNicknames = data['playerNicknames'] ?? [];
 
                           return Column(
                             children: [
+                              SizedSpacer.vertical(space: Space.small),
                               ListView.builder(
                                 physics: const NeverScrollableScrollPhysics(),
                                 shrinkWrap: true,
@@ -85,15 +102,16 @@ class _GameRoomPageState extends State<GameRoomPage> {
                                 }),
                               ),
                               SizedSpacer.vertical(space: Space.medium),
-                              if (playerNicknames.length < 4)
+                              if (playerNicknames.length < 2) ...[
                                 Text(
                                   'Waiting for players...',
                                   style: TextStyle(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary),
+                                    color:
+                                        Theme.of(context).colorScheme.onPrimary,
+                                  ),
                                 ),
-                              SizedSpacer.vertical(space: Space.medium),
+                                SizedSpacer.vertical(space: Space.small)
+                              ],
                             ],
                           );
                         } else {
@@ -107,10 +125,40 @@ class _GameRoomPageState extends State<GameRoomPage> {
                 ),
               ),
               SizedSpacer.vertical(space: Space.medium),
-              PrimaryGameButton(
-                label: 'Start Game',
-                maxSize: true,
-                onPressed: () {},
+              StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                stream: FirebaseFirestore.instance
+                    .collection("rooms")
+                    .doc(args.roomId)
+                    .snapshots(),
+                builder: (_, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error = ${snapshot.error}');
+                  } else if (snapshot.hasData) {
+                    var data = snapshot.data!.data();
+                    var playerNicknames = data!['playerNicknames'] ?? [];
+                    bool isPlayerEnough = playerNicknames.length > 1;
+
+                    return PrimaryGameButton(
+                      label: 'Start Game',
+                      maxSize: true,
+                      onPressed: isPlayerEnough
+                          ? () {
+                              Navigator.pushNamedAndRemoveUntil(
+                                context,
+                                GamePage.routeName,
+                                (route) => false,
+                                arguments: GamePageArguments(
+                                    args.roomId, args.nickname),
+                              );
+                            }
+                          : null,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
               ),
             ],
           ),
