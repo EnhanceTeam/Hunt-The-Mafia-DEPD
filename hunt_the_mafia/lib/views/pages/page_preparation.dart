040b24aba827f800ac665aaaf2b9bf863c312d1d
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2022-12-31 08:00:57 +0700

    Add Preparation Page

--- hunt_the_mafia/lib/views/pages/page_preparation.dart
+++ hunt_the_mafia/lib/views/pages/page_preparation.dart
@@ -10,9 +10,13 @@ class PreparationPage extends StatefulWidget {
 }
 
 class _PreparationPageState extends State<PreparationPage> {
+  int _civilianCount = 2;
+  int _mafiaCount = 1;
+  int _mrWhiteCount = 0;
+  int _mrBlackCount = 0;
+
   @override
   Widget build(BuildContext context) {
-    int _count = 0;
     return Scaffold(
       body: SafeArea(
         minimum: const EdgeInsets.all(Space.large),
@@ -37,37 +41,176 @@ class _PreparationPageState extends State<PreparationPage> {
                   ],
                 ),
               ),
-              SizedSpacer.vertical(space: Space.medium),
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      _count++;
-                    });
-                  },
-                  backgroundColor: Colors.white,
-                  child: const Icon(
-                    Icons.add,
-                    color: Colors.black,
-                  ),
+              SizedSpacer.vertical(space: Space.large),
+              Center(
+                child: Column(
+                  children: [
+                    const Text('Civilian',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (_civilianCount > 2) {
+                                _civilianCount--;
+                              }
+                            });
+                          },
+                          child: const Icon(Icons.remove, color: Colors.black),
+                        ),
+                        Text('$_civilianCount',
+                            style: const TextStyle(fontSize: 24.0)),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _civilianCount++;
+                            });
+                          },
+                          child: const Icon(Icons.add, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                Text('$_count', style: const TextStyle(fontSize: 60.0)),
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      _count--;
-                    });
-                  },
-                  backgroundColor: Colors.white,
-                  child: const Icon(Icons.remove,
-                      color: Colors.black),
+              ),
+              SizedSpacer.vertical(space: Space.large),
+              Center(
+                child: Column(
+                  children: [
+                    const Text('Mafia',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (_mafiaCount > 1) {
+                                _mafiaCount--;
+                              }
+                            });
+                          },
+                          child: const Icon(Icons.remove, color: Colors.black),
+                        ),
+                        Text('$_mafiaCount',
+                            style: const TextStyle(fontSize: 24.0)),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _mafiaCount++;
+                            });
+                          },
+                          child: const Icon(Icons.add, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          ),
+              ),
+              SizedSpacer.vertical(space: Space.large),
+              Center(
+                child: Column(
+                  children: [
+                    const Text('Mr. White',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (_mrWhiteCount > 0) {
+                                _mrWhiteCount--;
+                              }
+                            });
+                          },
+                          child: const Icon(Icons.remove, color: Colors.black),
+                        ),
+                        Text('$_mrWhiteCount',
+                            style: const TextStyle(fontSize: 24.0)),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _mrWhiteCount++;
+                            });
+                          },
+                          child: const Icon(Icons.add, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedSpacer.vertical(space: Space.large),
+              Center(
+                child: Column(
+                  children: [
+                    const Text('Mr. Black',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              if (_mrBlackCount > 0) {
+                                _mrBlackCount--;
+                              }
+                            });
+                          },
+                          child: const Icon(Icons.remove, color: Colors.black),
+                        ),
+                        Text('$_mrBlackCount',
+                            style: const TextStyle(fontSize: 24.0)),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(24, 48),
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _mrBlackCount++;
+                            });
+                          },
+                          child: const Icon(Icons.add, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedSpacer.vertical(space: Space.large),
+              FilledButton(
+                label: 'Start Game',
+                maxSize: true,
+                onPressed: () {},
+              )
             ],
           ),
         ),
