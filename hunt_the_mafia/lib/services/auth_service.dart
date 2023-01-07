9372d097c6668f83bdfb51b6143fb9de1c2d92f3
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-01-07 21:15:43 +0700

    update auth service

--- hunt_the_mafia/lib/services/auth_service.dart
+++ hunt_the_mafia/lib/services/auth_service.dart
@@ -1,13 +1,24 @@
 part of 'services.dart';
 
 class AuthService {
-
   static FirebaseAuth auth = FirebaseAuth.instance;
+
+  handleAuthState() {
+    return StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, snapshot) {
+          if (snapshot.hasData) {
+            return const ProfilePage();
+          } else {
+            return const MainMenuPage();
+          }
+        });
+  }
 
   static Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication googleAuth =
-    await googleUser!.authentication;
+        await googleUser!.authentication;
 
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
@@ -19,7 +30,8 @@ class AuthService {
 
   static Future signOut() async {
     await FirebaseAuth.instance.signOut();
-    await GoogleSignIn().signOut().then((value){
+    await GoogleSignIn().signOut().then((value) {
+      print("User Signed Out");
     });
   }
 }
