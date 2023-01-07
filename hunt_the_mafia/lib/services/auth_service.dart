22b0b22d0aa79c24b0d3bc4f971c3783e532be12
Daniel Aprillio <70996943+Nielioo@users.noreply.github.com>
2023-01-07 20:31:30 +0700

    add sign in and sign out function

--- hunt_the_mafia/lib/services/auth_service.dart
+++ hunt_the_mafia/lib/services/auth_service.dart
@@ -1,1 +1,25 @@
 part of 'services.dart';
+
+class AuthService {
+
+  static FirebaseAuth auth = FirebaseAuth.instance;
+
+  static Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleAuth =
+    await googleUser!.authentication;
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await auth.signInWithCredential(credential);
+  }
+
+  static Future signOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut().then((value){
+    });
+  }
+}
--- hunt_the_mafia/lib/services/services.dart
+++ hunt_the_mafia/lib/services/services.dart
@@ -1,9 +1,11 @@
 import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hunt_the_mafia/views/pages/pages.dart';
 import 'package:hunt_the_mafia/views/widgets/widgets.dart';
 
